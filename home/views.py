7dddb6c8433f593058748e2ce528b7b7a8c28b5d
clackbib <hokanla@spsu.edu>
2013-09-21 20:25:33 -0400

    * Improved CSS organization, Started home dashboard design.

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 
-#@login_required(login_url="login.views.connect")
+@login_required(login_url="login.views.connect")
 def home(request):
     return render(request,'home/home.html',locals())
