e227a5171cb226820fc26331a4b4b2e0fa0efe09
clackbib <hokanla@spsu.edu>
2013-09-24 16:55:01 -0400

    * Added Messaging App* *Added PrivateMessage model*

--- messaging/models.py
+++ messaging/models.py
@@ -4,4 +4,12 @@ from login.models import Profile
 
 class PrivateMessage(models.Model):
             sender = models.ForeignKey(Profile)
-            recie
+            receiver = models.ForeignKey(Profile)
+            date_sent = models.DateTimeField(auto_now_add=True)
+            content = models.TextField()
+            def __unicode__(self):
+                return " Message sent by {0} to {1} on {2}".format(self.sender, self.receiver, self.date_sent)
+
+
+
+
