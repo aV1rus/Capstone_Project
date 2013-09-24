dc9aa61d86ec623a5ab57b1c8ebfd34272fa093b
clackbib <hokanla@spsu.edu>
2013-09-24 18:57:58 -0400

    * Fixed the CSS static issue. *

--- capstone/settings.py
+++ capstone/settings.py
@@ -72,19 +72,20 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = 'static/' ## USE FOR LOCAL MACHINE
+STATIC_URL = '/static/' ## USE FOR LOCAL MACHINE
 # STATIC_URL = 'http://av1rus.dyndns.org/capstone_static/' ## ONLY HAVE THIS FOR DEPLOYMENT
-BASE_DIR = os.path.dirname(os.path.abspath("/var/www/capstone_static"))
+BASE_DIR = '' #Put base directory here.
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # os.path.join(BASE_DIR, "static"),
-    BASE_DIR,
     "static",
-    "/var/www/capstone_static"
+    "/var/www/capstone_static",
+    "C:/Users/Habib/PycharmProjects/Capstone_Project/static",
+
+
 )
 
 # List of finder classes that know how to find static files in
