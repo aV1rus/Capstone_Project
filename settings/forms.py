e5bdbff17e27ba3f4f8f381e0ae632f8f795f622
clackbib <hokanla@spsu.edu>
2013-09-26 16:46:35 -0400

    *Settings Page Started *

--- settings/forms.py
+++ settings/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth.models import User
 from login.models import Profile
 
+class SettingsFom(forms.ModelForm):
 
+    class Meta:
+        model = User
--- settings/views.py
+++ settings/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from django.shortcuts import render, redirect
+from settings.forms import SettingsFom
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="login.views.connect")
 def settings(request):
+    form = SettingsFom(initial={'first_name':request.user.first_name})
     return render(request, "home/settings/settings.html", locals())
