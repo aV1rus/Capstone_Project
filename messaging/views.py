c64bbaf114b6258d0574e8c1d15b19d967e4a4f0
clackbib <hokanla@spsu.edu>
2013-10-04 10:28:10 -0400

    *Fiew design changes *

--- messaging/views.py
+++ messaging/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="login.views.connect")
 def messaging(request):
-    return render_to_response('home/messaging/messaging.html', {'first_name': request.user.first_name})
+    return render_to_response('home/messaging/messaging.html',locals())
 
 # class MessagingTemp(TemplateView):
 #     template_name = 'hello_class.html'
