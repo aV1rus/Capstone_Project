f44f30343d8662eed23a24edcbbbab20c8e4642c
Nick M <Nick.Maiello@ncr.com>
2013-10-08 21:54:21 -0400

    Added Profile Stuff

--- sections/user_profile/views.py
+++ sections/user_profile/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @login_required(login_url="login.views.connect")
 def userProfile(request):
+    if request.method == 'GET':
+        if request.GET:
+            #TODO :: link to Profile instead of user
+            user_id = request.GET['userId']
+            user = User.objects.get(id=user_id)
+
     return render_to_response('home/user_profile/profile.html', locals())
