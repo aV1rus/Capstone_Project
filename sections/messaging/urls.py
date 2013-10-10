505a2f7f18ee5384e898ecde604f9574bedf0ecd
clackbib <hokanla@spsu.edu>
2013-10-10 01:39:58 -0400

    *Delete messages implemented. fiew tweaks.

--- sections/messaging/urls.py
+++ sections/messaging/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('sections.messaging.views',
    url(r'^compose/', 'compose'),
    url(r'^view/(?P<message_id>\d+)/', 'view'),
    url(r'^compose/(?P<receiver>\d+)/', 'compose'),
+   url(r'^delete/(?P<message_id>\d+)/', 'delete'),
    url(r'^outbox/','outbox'),
 
 )
