154a226ccf084253fb224c0f622c6cf16434a768
clackbib <hokanla@spsu.edu>
2013-10-14 17:56:42 -0400

    *Fixes and CSS*

--- sections/settings/forms.py
+++ sections/settings/forms.py
@@ -5,11 +5,13 @@ from sections.user_profile.models import Profile
 
 class SettingsForm(forms.ModelForm):
 
-    first_name = forms.CharField(max_length=30, required=False)
-    last_name = forms.CharField(max_length=30, required= False)
-    email = forms.EmailField(required=False)
+    first_name = forms.CharField(max_length=30, required=False,widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30, required= False,widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    email = forms.EmailField(required=False,widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    headline = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Profile Headline'}),required=False)
+
 
     class Meta:
         model = Profile
-        exclude = ('user',)
+        exclude = ('user','headline',)
 
